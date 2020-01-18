92ecd15847f0d64a8f150ce0628a2484b64f94fd
Damilola Ipadeola <damilolaipadeola23@gmail.com>
2020-01-18 08:27:59 +0100

    fix heroku build issue

--- src/Analysis.jsx
+++ src/Analysis.jsx
@@ -1,47 +1,49 @@
 import React, {Component} from 'react';
 import CanvasJSReact from './canvasjs.react';
 
-const CanvasJS = CanvasJSReact.CanvasJS;
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
-const options = {
-    animationEnabled: true,
-    subtitles: [{
-        text: "75%",
-        verticalAlign: "center",
-        fontSize: 38,
-        dockInsidePlotArea: true
-    }],
-    data: [{
-        type: "doughnut",
-        showInLegend: true,
-        dataPoints: [
-            { name: "Unsatisfied", y: 5 },
-            { name: "Very Unsatisfied", y: 31 },
-            { name: "Very Satisfied", y: 40 },
-            { name: "Satisfied", y: 17 },
-            { name: "Neutral", y: 7 }
-        ]
-    }]
-}
 
-const barOptions = {
-    data: [
-    {
-        type: "column",
-        dataPoints: [
-            { label: "Apple",  y: 10  },
-            { label: "Orange", y: 15  },
-            { label: "Banana", y: 25  },
-            { label: "Mango",  y: 30  },
-            { label: "Grape",  y: 28  }
-        ]
-    }
-    ]
-}
 class Analysis extends Component {
+
+    
     render() {
+        const options = {
+            animationEnabled: true,
+            subtitles: [{
+                text: "75%",
+                verticalAlign: "center",
+                fontSize: 38,
+                dockInsidePlotArea: true
+            }],
+            data: [{
+                type: "doughnut",
+                showInLegend: true,
+                dataPoints: [
+                    { name: "Unsatisfied", y: 5 },
+                    { name: "Very Unsatisfied", y: 31 },
+                    { name: "Very Satisfied", y: 40 },
+                    { name: "Satisfied", y: 17 },
+                    { name: "Neutral", y: 7 }
+                ]
+            }]
+        }
+        
+        const barOptions = {
+            data: [
+            {
+                type: "column",
+                dataPoints: [
+                    { label: "Apple",  y: 10  },
+                    { label: "Orange", y: 15  },
+                    { label: "Banana", y: 25  },
+                    { label: "Mango",  y: 30  },
+                    { label: "Grape",  y: 28  }
+                ]
+            }
+            ]
+        }
         return (
             <div className="bg-white rounded my-2 mx-2 p-2 lg:my-6 lg:mx-8 lg:p-6">
                 <div className="block lg:flex mb-6">
@@ -81,14 +83,14 @@ class Analysis extends Component {
                 </div>
                 <div className="block lg:flex">
                     <div className="lg:flex-initial mr-4 lg:w-1/3">
-                        <div className="rounded-lg bg-white shadow-md p-2 lg:p-8 text-center">
+                        <div className="rounded-lg bg-white shadow-lg p-2 lg:p-8 text-center">
                             <div className="text-gray-600 font-semibold">Saving Pattern</div>
                             <CanvasJSChart options = {options}/>
                         </div>
         
                     </div>
                     <div className="flex-auto">
-                    <div className="rounded-lg bg-white shadow-md p-2 lg:p-8">
+                    <div className="rounded-lg bg-white shadow-lg p-2 lg:p-8">
                         <div>
 
                         </div>
